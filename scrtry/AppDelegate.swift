17b74e6cfef07203d1ef31cbcf38be4a40a8f149
badi3 <mo@badi3.com>
2019-10-05 19:12:24 +0200

    Initial Commit

---
+++ scrtry/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  scrtry
+//
+//  Created by badi3 on 10/5/19.
+//  Copyright © 2019 Badi3. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ scrtry/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  scrtry
+//
+//  Created by badi3 on 10/5/19.
+//  Copyright © 2019 Badi3. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
